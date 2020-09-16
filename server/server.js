55cce7f1dd01ac58fab91b94e4aca6668b543446
rinaykumar <rkumar4@mail.sfsu.edu>
2020-09-15 22:24:20 -0700

    createListing and viewListings

--- server/server.js
+++ server/server.js
@@ -1,21 +1,38 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const {nanoid} = require('nanoid');
 const app = express();
 
-app.use(express.json()) // Middleware for json
+app.use(express.json())
+app.use(bodyParser.json());
+
+// Items database
+let items = [];
 
 // Root endpoint, sucess: fails and errorCode: 404
 app.get('/', (req, res) => {
   res.json({sucess: false, items: null, inquiries: null, errorCode: 404});
 });
 
-// Test endpoint
-app.get('/test', (req, res) => {
-  res.send('test');
+// /api/createListing (post)
+app.post('/api/createListing', (req, res) => {
+  let item = new Object();
+  
+  item.description = req.body.description;
+  item.type = req.body.type;
+  item.price = req.body.price;
+  item.title = req.body.title;
+  item.id = nanoid(8);
+
+  items.push(item);
+
+  res.json({sucess: true, items: items, inquiries: null, errorCode: null});
 });
 
-// /api/createListing (post)
-
 // /api/viewListings
+app.get('/api/viewListings', (req, res) => {
+  res.json({sucess: true, items: items, inquiries: null, errorCode: null});
+});
 
 // /api/viewListings?type=<type>
 
